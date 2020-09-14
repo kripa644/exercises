d61346d8a282df937588557b977c0bda086e23ed
kripa644 <kripa@robosoftin.com>
2020-09-14 19:53:46 +0530

    Chessboard exercise

--- chapter-02/chessboard.js
+++ chapter-02/chessboard.js
@@ -1,14 +1,14 @@
-let space = "";
+let patternLine = "";
 let size = 8;
-for (let i = 1; i <= size; i++)
-{
-    for (let j = 1; j <= size; j++)
-    {
-        if ((i + j) % 2 === 0)
-            space += " ";
-        else
-            space += "#";
+for (let i = 1; i <= size; i++) {
+    for (let j = 1; j <= size; j++) {
+        if ((i + j) % 2 === 0) {
+            patternLine += " ";
+        }
+        else {
+            patternLine += "#";
+        }
     }   
-    space += "\n";
+    patternLine += "\n";
 }
-console.log(space);
+console.log(patternLine);
