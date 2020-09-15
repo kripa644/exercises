ceec44a53decc32d9d04c98cbbebefa6c1188e4d
kripa644 <kripa@robosoftin.com>
2020-09-15 12:14:20 +0530

    Modified oddsum file

--- Exercises/oddsum.js
+++ Exercises/oddsum.js
@@ -2,12 +2,9 @@ let oddSum = function (n) {
     if (n % 2 == 0) {
         console.log("The number is even");
     }
-    else if (n == 1) {
-        return 1;
-    }
     else {
-        return n + oddSum(n-2);
+        return n == 1 ? 1 : n + oddSum(n-2);
     }
 };
 
-console.log(oddSum(7));
+console.log(oddSum(5));
